a7c4046cec30a4cf2e4ad61287a12136ac0a2caa
Calder Niven <calderjeniven@outlook.com>
2021-02-01 22:31:00 +0000

    disabled notification spam

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,3 +2,5 @@ let mix = require('laravel-mix');
 
 mix.js('src/js/app.js', 'dist').setPublicPath('dist');
 mix.sass('src/sass/app.scss', 'dist').setPublicPath('dist');
+
+mix.disableSuccessNotifications();
